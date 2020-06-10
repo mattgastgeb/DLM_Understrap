87344188ab8a68f2653c2e7912cb460e50f56923
Matt's Macbook <matt@Mattss-MacBook-Pro.local>
2020-06-09 22:27:54 -0400

    finished styling theme and homepage

---
+++ page-templates/custom-page.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Custom Full Width Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<?php if ( is_front_page() ) : ?>
+  <?php get_template_part( 'global-templates/hero' ); ?>
+<?php endif; ?>
+
+
+<div class="wrapper" id="full-width-page-wrapper">
+
+  <main class="site-main" id="main" role="main">
+
+      <?php while ( have_posts() ) : the_post(); ?>
+
+        <?php get_template_part( 'loop-templates/content', 'custompage' ); ?>
+
+      <?php endwhile; // end of the loop. ?>
+
+  </main><!-- #main -->
+
+</div><!-- #full-width-page-wrapper -->
+
+<?php get_footer(); ?>
